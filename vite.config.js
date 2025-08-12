8c972a81180c0c2c96ae20c174d15d3122a8c0de
RonKat2008 <108700006+RonKat2008@users.noreply.github.com>
2025-08-11 22:48:22 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "/ronit-cs4-quiz-project",
+  base: "/ronit-cs4-quiz-project",
 })
